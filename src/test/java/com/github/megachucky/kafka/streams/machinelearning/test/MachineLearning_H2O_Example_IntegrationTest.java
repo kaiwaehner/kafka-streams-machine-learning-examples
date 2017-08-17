e33f037bc04dfd2e0c7a1b485db3945bf678a6af
Kai Waehner <kai.waehner@Kai-Waehner.fritz.box>
2017-08-17 13:36:17 +0200

    javadoc updated

--- src/test/java/com/github/megachucky/kafka/streams/machinelearning/test/MachineLearning_H2O_Example_IntegrationTest.java
+++ src/test/java/com/github/megachucky/kafka/streams/machinelearning/test/MachineLearning_H2O_Example_IntegrationTest.java
@@ -1,17 +1,3 @@
-/**
- * Copyright 2016 Confluent Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package  com.github.megachucky.kafka.streams.machinelearning.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +28,9 @@ import hex.genmodel.easy.exception.PredictException;
 import hex.genmodel.easy.prediction.BinomialModelPrediction;
 
 /**
+ * 
+ * @author Kai Waehner (www.kai-waehner.de)
+ * 
  * End-to-end integration test based on {@link MachineLearning_H2O_Example}, using an
  * embedded Kafka cluster and a H2O.ai GBM Model.
  *

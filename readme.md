77fb864da7b5e7ec1f4ce0b09934bf60515501b8
Kai Waehner <kai.waehner@Kai-Waehner.local>
2017-08-25 10:56:51 +0200

    Added TensorFlow CNN Example for image recognition

--- readme.md
+++ readme.md
@@ -4,14 +4,26 @@ This project contains **examples which demonstrate how to deploy analytic models
 Examples will include analytic models built with TensorFlow, Keras, H2O, Python, DeepLearning4J and other technologies.
 
 ## Installation and Usage
+Java 8 and Maven 3 are required. Maven will download all required dependencies.
+
 Just download the project and run 'mvn clean package'.
 
 Every examples includes a implementation and a unit test. The project and examples are very simple and lightweight. No further configuration is needed to build and run it.
 
-Java 8 and Maven 3 are required. Maven will download all required dependencies.
+The unit test use some helper classes like EmbeddedSingleNodeKafkaCluster in package "com.github.megachucky.kafka.streams.machinelearning.test.utils". If you want to run the main classes, you need to start
+a Kafka cluster (with at least one Zookeeper and one Kafka broker running) and create the required topics.
 
 ## Use Cases and Technologies
-This project kicks off with an example of using Kafka Streams to deploy a H2O GBM model. More sophisticated use cases and other technologies will be added over time.
+This examples are already available including unit tests:
+* Deployment of a H2O GBM model to a Kafka Streams application for prediction of flight delay
+* Deployment of a pre-built TensorFlow CNN model for image recognition.
+
+More sophisticated use cases and other technologies will be added over time. The following will be added as soon as I find some more spare time (all examples will be built into a Kafka Streams environment ):
+* Anomaly detection using Autoencoders leveraging DeepLearning4J.
+* Image Recognition using H2O (to show the difference of H2O instead of using just low level TensorFlow APIs)
+* Cross Selling and Customer Churn Detection using classical Machine Learning algorithms but also Deep Learning
+* Stateful stream processing to combine different model execution steps into an even more powerful workflow than just inferencing single events (e.g. using sliding or session windows).
+* Keras to build different models with Python, TensorFlow, Theano and other Deep Learning frameworks under the hood + Kafka Streams as generic Machine Learning infrastructure to deploy, execute and monitor these different models.
 
 ### Example 1 - Gradient Boosting with H2O.ai for Prediction of Flight Delays
 **Use Case**
@@ -32,3 +44,25 @@ H2O generated GBM Java model is instantiated and used in a Kafka Streams applica
 **Unit Test**
 
 [MachineLearning_H2O_Example_IntegrationTest.java](https://github.com/kaiwaehner/kafka-streams-machine-learning-examples/blob/master/src/test/java/com/github/megachucky/kafka/streams/machinelearning/test/Kafka_Streams_MachineLearning_H2O_GBM_Example_IntegrationTest.java)
+
+### Example 2 - Convolutional Neural Network (CNN) with TensorFlow for Image Recognition
+**Use Case**
+
+Convolutional Neural Network (CNN) to for image recognition.
+A prebuilt TensorFlow CNN model is instantiated and used in a Kafka Streams application to do recognize new JPEG images. A Kafka Input Topic receives the location of a new images (another option would be to send the image in the Kafka message instead of just a link to it), infers the content of the picture via the TensorFlow model, and sends the result to a Kafka Output Topic.
+
+**Machine Learning Technology**
+* TensorFlow (TODO Link)
+* Leverages TensorFlow for Java (https://www.tensorflow.org/install/install_java). These APIs are particularly well-suited to loading models created in Python and executing them within a Java application. The Java API doesn't yet include convenience functions (which you might know from Keras), thus a private helper class is used in the example for construction and execution of the pre-built TensorFlow model.
+* Check the official TensorFlow demo 'LabelImage' to understand this image recognition example
+* https://github.com/kaiwaehner/tensorflow/blob/r1.3/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java
+* You can re-use the pre-trained TensorFlow model attached to this project (tensorflow_inception_graph.pb,  more details here: http://arxiv.org/abs/1512.00567)) or add your own model.
+* The 'images' folder contains models which were used for training the model (trained_airplane_1.jpg, trained_airplane_2.jpg, trained_butterfly.jpg) but also a new picture (new_airplane.jpg) which is not known by the model and using a different resolution than the others. Feel free to add your own pictures (they need to be trained, see list of trained pictures in the file: imagenet_comp_graph_label_strings.txt), otherwise the model will return 'unknown'.
+
+**Source Code**
+
+[Kafka_Streams_TensorFlow_Image_Recognition_Example.java](TODO)
+
+**Unit Test**
+
+[Kafka_Streams_TensorFlow_Image_Recognition_Example_IntegrationTest.java](TODO)
---
+++ src/main/java/com/github/megachucky/kafka/streams/machinelearning/Kafka_Streams_TensorFlow_Image_Recognition_Example.java
@@ -0,0 +1,263 @@
+package com.github.megachucky.kafka.streams.machinelearning;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.ForeachAction;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.tensorflow.DataType;
+import org.tensorflow.Graph;
+import org.tensorflow.Output;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
+/**
+ * @author Kai Waehner (www.kai-waehner.de)
+ * 
+ *         Creates a new Kafka Streams application for Image Recognition. 
+ *         The application uses the CNN model "inception5h" (built with
+ *         TensorFlow) to infer messages sent to Kafka topic "ImageInputTopic".
+ *         The outcome of model inference is sent to Kafka topic
+ *         "ImageOutputTopic".
+ *
+ */
+public class Kafka_Streams_TensorFlow_Image_Recognition_Example {
+
+	private static final String imageInputTopic = "ImageInputTopic";
+	private static final String imageOutputTopic = "ImageOutputTopic";
+	
+	
+	// Prediction Value
+	private static String imageClassification = "unknown";
+	private static String imageProbability = "unknown";
+
+	public static void main(final String[] args) throws Exception {
+			
+			// Create TensorFlow object
+		
+		    String modelDir = "src/main/resources/generatedModels/CNN_inception5h";
+		    
+		    Path pathGraph = Paths.get(modelDir, "tensorflow_inception_graph.pb");
+		    byte[] graphDef = Files.readAllBytes(pathGraph);
+		    		    
+		    Path pathModel = Paths.get(modelDir, "imagenet_comp_graph_label_strings.txt");
+		    List<String> labels = Files.readAllLines(pathModel, Charset.forName("UTF-8"));
+		      
+		   
+
+			// Configure Kafka Streams Application
+			final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
+			final Properties streamsConfiguration = new Properties();
+			// Give the Streams application a unique name. The name must be unique
+			// in the Kafka cluster
+			// against which the application is run.
+			streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-tensorflow-image-recognition-example");
+			// Where to find Kafka broker(s).
+			streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+			
+		
+			// Specify default (de)serializers for record keys and for record
+			// values.
+			streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+			streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+			
+			// In the subsequent lines we define the processing topology of the
+			// Streams application.
+			final KStreamBuilder builder = new KStreamBuilder();
+
+			// Construct a `KStream` from the input topic "ImageInputTopic", where
+			// message values represent lines of text 
+			final KStream<String, String> imageInputLines = builder.stream(imageInputTopic);
+
+			
+			// Stream Processor (in this case 'foreach' to add custom logic, i.e. apply the analytic model)
+			imageInputLines.foreach(new ForeachAction<String, String>() {
+				public void apply(String key, String value) {
+
+					imageClassification = "unknown";
+					imageProbability = "unknown";
+					
+					String imageFile = value;
+
+				    Path pathImage = Paths.get(imageFile);
+				    byte[] imageBytes;
+					try {
+						imageBytes = Files.readAllBytes(pathImage);
+						
+						try (Tensor image = constructAndExecuteGraphToNormalizeImage(imageBytes)) {
+						      float[] labelProbabilities = executeInceptionGraph(graphDef, image);
+						      int bestLabelIdx = maxIndex(labelProbabilities);
+						      
+						      
+						      imageClassification = labels.get(bestLabelIdx);
+						      
+						      imageProbability = Float.toString(labelProbabilities[bestLabelIdx] * 100f);
+						      
+						      System.out.println(
+						          String.format(
+						              "BEST MATCH: %s (%.2f%% likely)",
+						              imageClassification, labelProbabilities[bestLabelIdx] * 100f));
+						    }
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+
+	// airlineInputLines.print();
+
+	// Transform message: Add prediction information
+	KStream<String, Object> transformedMessage = imageInputLines
+			.mapValues(value -> "Prediction: What is the content of this picture? => " + imageClassification + ", probability = " + imageProbability);
+
+	// Send prediction information to Output Topic
+	transformedMessage.to(imageOutputTopic);
+
+	// Start Kafka Streams Application to process new incoming images from the Input Topic
+	final KafkaStreams streams = new KafkaStreams(builder,
+			streamsConfiguration);
+	
+	streams.cleanUp();
+	
+	streams.start();
+	
+	System.out.println("Image Recognition Microservice is running...");
+	
+	System.out.println("Input to Kafka Topic " + imageInputTopic + "; Output to Kafka Topic " + imageOutputTopic);
+
+	// Add shutdown hook to respond to SIGTERM and gracefully close Kafka
+	// Streams
+	Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+	}
+
+	// ########################################################################################
+	// Private helper class for construction and execution of the pre-built TensorFlow model
+	// ########################################################################################
+
+	private static Tensor constructAndExecuteGraphToNormalizeImage(byte[] imageBytes) {
+		//Graph construction: using the OperationBuilder class to construct a graph to decode, resize and normalize a JPEG image.
+		
+		try (Graph g = new Graph()) {
+			GraphBuilder b = new GraphBuilder(g);
+			// Some constants specific to the pre-trained model at:
+			// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+			//
+			// - The model was trained with images scaled to 224x224 pixels.
+			// - The colors, represented as R, G, B in 1-byte each were
+			// converted to
+			// float using (value - Mean)/Scale.
+			final int H = 224;
+			final int W = 224;
+			final float mean = 117f;
+			final float scale = 1f;
+
+			// Since the graph is being constructed once per execution here, we
+			// can use a constant for the
+			// input image. If the graph were to be re-used for multiple input
+			// images, a placeholder would
+			// have been more appropriate.
+			final Output input = b.constant("input", imageBytes);
+			final Output output = b
+					.div(b.sub(
+							b.resizeBilinear(b.expandDims(b.cast(b.decodeJpeg(input, 3), DataType.FLOAT),
+									b.constant("make_batch", 0)), b.constant("size", new int[] { H, W })),
+							b.constant("mean", mean)), b.constant("scale", scale));
+			try (Session s = new Session(g)) {
+				return s.runner().fetch(output.op().name()).run().get(0);
+			}
+		}
+	}
+
+	private static float[] executeInceptionGraph(byte[] graphDef, Tensor image) {
+		try (Graph g = new Graph()) {
+			
+			// Model loading: Using Graph.importGraphDef() to load a pre-trained Inception model.
+			g.importGraphDef(graphDef);
+			
+			// Graph execution: Using a Session to execute the graphs and find the best label for an image.
+			try (Session s = new Session(g);
+					Tensor result = s.runner().feed("input", image).fetch("output").run().get(0)) {
+				final long[] rshape = result.shape();
+				if (result.numDimensions() != 2 || rshape[0] != 1) {
+					throw new RuntimeException(String.format(
+							"Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+							Arrays.toString(rshape)));
+				}
+				int nlabels = (int) rshape[1];
+				return result.copyTo(new float[1][nlabels])[0];
+			}
+		}
+	}
+
+	private static int maxIndex(float[] probabilities) {
+		int best = 0;
+		for (int i = 1; i < probabilities.length; ++i) {
+			if (probabilities[i] > probabilities[best]) {
+				best = i;
+			}
+		}
+		return best;
+	}
+
+	// In the fullness of time, equivalents of the methods of this class should
+	// be auto-generated from
+	// the OpDefs linked into libtensorflow_jni.so. That would match what is
+	// done in other languages
+	// like Python, C++ and Go.
+	static class GraphBuilder {
+		GraphBuilder(Graph g) {
+			this.g = g;
+		}
+
+		Output div(Output x, Output y) {
+			return binaryOp("Div", x, y);
+		}
+
+		Output sub(Output x, Output y) {
+			return binaryOp("Sub", x, y);
+		}
+
+		Output resizeBilinear(Output images, Output size) {
+			return binaryOp("ResizeBilinear", images, size);
+		}
+
+		Output expandDims(Output input, Output dim) {
+			return binaryOp("ExpandDims", input, dim);
+		}
+
+		Output cast(Output value, DataType dtype) {
+			return g.opBuilder("Cast", "Cast").addInput(value).setAttr("DstT", dtype).build().output(0);
+		}
+
+		Output decodeJpeg(Output contents, long channels) {
+			return g.opBuilder("DecodeJpeg", "DecodeJpeg").addInput(contents).setAttr("channels", channels).build()
+					.output(0);
+		}
+
+		Output constant(String name, Object value) {
+			try (Tensor t = Tensor.create(value)) {
+				return g.opBuilder("Const", name).setAttr("dtype", t.dataType()).setAttr("value", t).build().output(0);
+			}
+		}
+
+		private Output binaryOp(String type, Output in1, Output in2) {
+			return g.opBuilder(type, type).addInput(in1).addInput(in2).build().output(0);
+		}
+
+		private Graph g;
+	}
+
+}
